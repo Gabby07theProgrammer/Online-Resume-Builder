7e2cf071f718778e092d6ba13976b084aadde054
Elijah Gabe Cervantes <eg.cervantes00210@student.tsu.edu.ph>
2024-11-17 22:06:09 +0800

    Updated Template page

---
+++ Template.js
@@ -0,0 +1,11 @@
+function FirstTemp(){
+    location.replace("Resume_Form.html")
+}
+
+function SecondTemp(){
+    location.replace("Resume_Form.html")
+}
+
+function ThirdTemp(){
+    location.replace("Resume_Form.html")
+}
