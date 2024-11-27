9dc8e281b700fe78b9ba700881d34401596ed5a0
Elijah Gabe Cervantes <eg.cervantes00210@student.tsu.edu.ph>
2024-11-27 21:16:29 +0800

    New Database Name

--- connect.php
+++ connect.php
@@ -3,9 +3,10 @@
 $host="localhost";
 $user="root";
 $pass="";
-$db="loginreg";
+$db="LogAndRegis";
 $conn=new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
     echo "Failed to connect DB".$conn->connect_error;
 }
+
 ?>
