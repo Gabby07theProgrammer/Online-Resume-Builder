4ad03bbe771a0e4414fbca1f5827700aa92e6cce
Elijah Gabe Cervantes <eg.cervantes00210@student.tsu.edu.ph>
2024-12-01 18:20:36 +0800

    New Files

--- User_Page.js
+++ User_Page.js
@@ -1,11 +1,11 @@
 function FirstTemp(){
-    location.replace("Resume_Form.php")
+    location.replace("Resume_Form-1.php")
 }
 
 function SecondTemp(){
-    location.replace("Resume_Form.php")
+    location.replace("Resume_Form-2.php")
 }
 
 function ThirdTemp(){
-    location.replace("Resume_Form.php")
+    location.replace("Resume_Form-3.php")
 }
